d61d3a513806ae9211dfd14b6e0520b554f9150c
Balaji <balaji.vishnubhotla@scadea.com>
2024-03-26 15:57:55 -0500

    Added Help and Account components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { SideNavComponent } from './core/components/side-nav/side-nav.component';
+import { HelpComponent } from './core/components/help/help.component';
+import { AccountComponent } from './core/components/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    HelpComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
