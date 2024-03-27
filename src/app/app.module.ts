3c12386f81d36cffe48f6201502d1152d6ed9c8a
Balaji <balaji.vishnubhotla@scadea.com>
2024-03-27 15:24:48 -0500

    Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './core/components/home/home.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
